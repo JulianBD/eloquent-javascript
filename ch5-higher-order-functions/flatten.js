12b114b0a200e896ceaa3315d5f1fc140a04dfae
Julian Dorsey <juliandorsey@fastmail.com>
2022-12-02 01:54:55 -0500

    Added ch5 exercises

---
+++ ch5-higher-order-functions/flatten.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+function flatten(arrays) {
+  return arrays.reduce((a, b) => a.concat(b));
+}
+console.log(flatten(arrays));
---
+++ ch5-higher-order-functions/loop.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function loop(n, test, update, action) {
+    while (test(n)) {
+    action(n);
+    n = update(n);
+    }
+}
+  
+loop(3, n => n > 0, n => n - 1, console.log);
